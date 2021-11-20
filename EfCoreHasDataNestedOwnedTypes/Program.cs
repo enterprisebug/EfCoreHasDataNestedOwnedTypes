01f8113e8a958e7a0b36cc42486134a29f2228da
Daniel Sirz 🚬 <Sirz_Daniel@cat.com>
2021-11-20 09:32:25 +0100

    cleanup

--- EfCoreHasDataNestedOwnedTypes/Program.cs
+++ EfCoreHasDataNestedOwnedTypes/Program.cs
@@ -6,25 +6,6 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        //var ctx = new TwoLevelDbContext();
-        //ctx.Database.EnsureDeleted();
-        //ctx.Database.EnsureCreated();
-
-        //var x = new RootEntity(
-        //    Guid.Parse("96e1d442-bdd0-4c6f-9d01-624b27abbac3"),
-        //    new OwnedEntityLevel1
-        //    {
-        //        Id = Guid.Parse("8f8eea73-0b43-412a-b0aa-a9338db6e067")
-        //        //OwnedEntityLevel2 = new OwnedEntityLevel2 { Id = Guid.Parse("10b5e414-ce2a-440c-af40-3cf41a6d9718"), MyProperty = "Hello 1" }
-        //    }
-        //);
-
-
-        //ctx.Add(x);
-        //var entry = ctx.Entry(x);
-        //var herp = ctx.Entry(x.OwnedEntityLevel1);
-        //var derp = ctx.Entry(x.OwnedEntityLevel1.OwnedEntityLevel2);
-
         Console.WriteLine("Hello World!");
     }
 }
--- README.md
+++ README.md
@@ -4,5 +4,6 @@ Minimal reproducible example
 #
 
 	dotnet ef migrations add Initial --context TwoLevelDbContext -o Migrations/TwoLevel
+	dotnet ef migrations remove --context TwoLevelDbContext
 
 	dotnet ef migrations add Initial --context ThreeLevelDbContext -o Migrations/ThreeLevel
